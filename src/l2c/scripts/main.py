6636fa8639ccd65d213ad8c5dfbc1d8e4864dcd5
seif <vateseif@gmail.com>
2024-01-29 03:32:01 +0100

    simplify moveit and create main

--- src/l2c/scripts/main.py
+++ src/l2c/scripts/main.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+import os
+import sys
+print(os.getcwd())
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'config'))
+
+
+import rospy
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+
+import tf.transformations
+
+import numpy as np
+from controller import Controller
+from config import ControllerConfig
+
+
+
+
+print(f"CONTROLLER CONFIG STATUS = {ControllerConfig.status}")
+
+def talker():
+    rospy.init_node('mpc', anonymous=True)
+    pub = rospy.Publisher('/target_pose', Pose, queue_size=5)
+    rate = rospy.Rate(5)  # 10 Hz
+
+    # Example pose: position (x, y, z) and orientation in Euler angles (roll, pitch, yaw)
+    position = Point(0.3, 0.0, 0.3)  # Substitute with actual values
+    euler = (pi, 0., -pi/4)
+    #quaternion = Quaternion(0.924, -0.383, 0., 0.)  # Substitute with actual values
+    #target_euler = tf.transformations.euler_from_quaternion((0.924, -0.383, 0., 0.))
+    #print(f"TARGET EULER = {target_euler}")
+    # Wait for the user to press Enter
+    input("Press Enter to publish the robot's pose...")
+    # Convert Euler angles to quaternion for orientation
+    quaternion = Quaternion(*tf.transformations.quaternion_from_euler(*euler))
+    # Create Pose message
+    
+    
+    while not rospy.is_shutdown():
+        t = rospy.Time.now().to_sec()
+        position = Point(0.3, 0.0, 0.4 + 0.1*sin(t))
+
+        pose = Pose(position, quaternion)
+
+        rospy.loginfo(pose)
+        pub.publish(pose)
+        rospy.loginfo("Pose published. Node will now shutdown.")
+        #rospy.signal_shutdown("Pose published")
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
--- src/l2c/scripts/robot.py
+++ src/l2c/scripts/robot.py
@@ -6,78 +6,24 @@ from geometry_msgs.msg import Pose
 from moveit_msgs.msg import DisplayTrajectory
 
 
-class MoveGroupPythonInterfaceTutorial(object):
-    """MoveGroupPythonInterfaceTutorial"""
+class Robot(object):
+    """Robot"""
 
     def __init__(self):
-        super(MoveGroupPythonInterfaceTutorial, self).__init__()
+        super(Robot, self).__init__()
 
-        ## BEGIN_SUB_TUTORIAL setup
         ##
         ## First initialize `moveit_commander`_ and a `rospy`_ node:
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
+        rospy.init_node("robot", anonymous=True)
 
-        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
-        ## kinematic model and the robot's current joint states
-        robot = moveit_commander.RobotCommander()
+        # init pubs and subs
+        self.pose_publisher = rospy.Publisher('/pose', Pose, queue_size=1)
+        self.target_pose_subscriber = rospy.Subscriber('/target_pose', Pose, self.go_to_pose)
 
-        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
-        ## for getting, setting, and updating the robot's internal understanding of the
-        ## surrounding world:
-        scene = moveit_commander.PlanningSceneInterface()
 
-        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
-        ## to a planning group (group of joints).  In this tutorial the group is the primary
-        ## arm joints in the Panda robot, so we set the group's name to "panda_arm".
-        ## If you are using a different robot, change this value to the name of your robot
-        ## arm planning group.
-        ## This interface can be used to plan and execute motions:
         group_name = "panda_arm"
-        move_group = moveit_commander.MoveGroupCommander(group_name)
-
-        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
-        ## trajectories in Rviz:
-        display_trajectory_publisher = rospy.Publisher(
-            "/move_group/display_planned_path",
-            DisplayTrajectory,
-            queue_size=20,
-        )
-
-        ## END_SUB_TUTORIAL
-
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
-        ## Getting Basic Information
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-        # We can get the name of the reference frame for this robot:
-        planning_frame = move_group.get_planning_frame()
-        print("============ Planning frame: %s" % planning_frame)
-
-        # We can also print the name of the end-effector link for this group:
-        eef_link = move_group.get_end_effector_link()
-        print("============ End effector link: %s" % eef_link)
-
-        # We can get a list of all the groups in the robot:
-        group_names = robot.get_group_names()
-        print("============ Available Planning Groups:", robot.get_group_names())
-
-        # Sometimes for debugging it is useful to print the entire state of the
-        # robot:
-        print("============ Printing robot state")
-        print(robot.get_current_state())
-        print("")
-        ## END_SUB_TUTORIAL
-
-        # Misc variables
-        self.box_name = ""
-        self.robot = robot
-        self.scene = scene
-        self.move_group = move_group
-        self.display_trajectory_publisher = display_trajectory_publisher
-        self.planning_frame = planning_frame
-        self.eef_link = eef_link
-        self.group_names = group_names
+        self.move_group = moveit_commander.MoveGroupCommander(group_name)
 
     def go_to_pose(self, ee_pose):
         """
@@ -96,34 +42,17 @@ class MoveGroupPythonInterfaceTutorial(object):
         self.move_group.clear_pose_targets()
 
         return True
-
-def main():
-    try:
-        print("")
-        print("----------------------------------------------------------")
-        print("Start listener ...")
-        print("----------------------------------------------------------")
-        print("")
-
-        tutorial = MoveGroupPythonInterfaceTutorial()
-
-        print(f"CURRENT POSE: {tutorial.move_group.get_current_pose()}")
-
-
-        # Initialize the node with the name 'listener_node'
-        #rospy.init_node('robot', anonymous=True)
-        
-        # Create a subscriber object that subscribes to the 'chatter' topic
-        rospy.Subscriber("/pose", Pose, tutorial.go_to_pose, queue_size=10)
-        
-        # Keep python from exiting until this node is stopped
-        rospy.spin()
-
-    except rospy.ROSInterruptException:
-        return
-    except KeyboardInterrupt:
-        return
-
+    
+    def get_pose(self, req):
+        return self.move_group.get_current_pose.pose
+    
+    def run(self):
+        rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            # publish current pose
+            self.pose_publisher.publish(self.get_pose())
+            rate.sleep()
 
 if __name__ == "__main__":
-    main()
+    robot = Robot()
+    robot.run()
