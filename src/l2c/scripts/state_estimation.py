e7c05e865a5c0ffe409bdea4db99be4621312cc8
seif <vateseif@gmail.com>
2024-01-30 15:43:48 +0100

    change urdf path

--- src/l2c/scripts/state_estimation.py
+++ src/l2c/scripts/state_estimation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import os
 import rospy
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import Pose, Twist, PoseStamped, TwistStamped
 
 import numpy as np
 import pinocchio as pin
-from pinocchio.utils import se3ToXYZQUAT
+#from pinocchio.utils import se3ToXYZQUAT
 
 class FrankaEEFInfoNode:
     def __init__(self):
@@ -13,7 +14,7 @@ class FrankaEEFInfoNode:
         rospy.init_node('state_estimation')
 
         # Load the URDF of the Franka robot (ensure the correct path)
-        self.robot_model = pin.buildModelFromUrdf('/home/seif/projects/l2c_sim/src/l2c/robots/panda.urdf')
+        self.robot_model = pin.buildModelFromUrdf(os.path.join(os.path.dirname(__file__), '..', 'robots/panda.urdf'))
         self.robot_data = self.robot_model.createData()
 
         # subs
@@ -36,7 +37,7 @@ class FrankaEEFInfoNode:
 
         # Convert the pose to a ROS Pose message
         pose = Pose()
-        xyz_quat = se3ToXYZQUAT(eef_pose)
+        xyz_quat = pin.SE3ToXYZQUAT(eef_pose)
         pose.position.x, pose.position.y, pose.position.z = (xyz_quat[0], xyz_quat[1], xyz_quat[2])
         pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w = (xyz_quat[3], xyz_quat[4], xyz_quat[5], xyz_quat[6])
 
@@ -61,6 +62,7 @@ class FrankaEEFInfoNode:
         # publish
         self.ee_pose_publisher.publish(pose_msg)
         self.ee_velocity_publisher.publish(twist_msg)
+        rospy.loginfo("OK")
 
 if __name__ == '__main__':
     node = FrankaEEFInfoNode()
