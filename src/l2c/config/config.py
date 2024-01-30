5893a12fdba8324f27941abe7262c0962f814c8e
seif <vateseif@gmail.com>
2024-01-30 18:10:36 +0100

    add llm

--- src/l2c/config/config.py
+++ src/l2c/config/config.py
@@ -1,4 +1,13 @@
 
+class LLMConfig:
+    mock_task = None#"OD_move_table"
+    prompt: str = "" # TODO: this is bad. Only works for NMPC now
+    avatar: str = "OD"
+    parsing: str = "optimization"
+    model_name: str = "gpt-3.5-turbo"
+    streaming: bool = False # whether you want to stream text to the streamlit interface
+    temperature: float = 0.6
+
 class ControllerConfig:
     nx: int = 3
     nu: int = 3 
